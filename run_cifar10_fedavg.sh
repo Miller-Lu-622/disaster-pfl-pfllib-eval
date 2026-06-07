98740a6b2bb075c2a31e0e1ec629f958b95fa2b2
Miller Lu <millerlu@Millers-MacBook-Air-2.local>
2026-06-07 21:07:09 +1200

    Add UoA cluster run script and successful FedAvg log

---
+++ run_cifar10_fedavg.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=pfllib-c10
+#SBATCH --time=02:00:00
+#SBATCH --open-mode=append
+#SBATCH --output=pfllib-c10.out
+#SBATCH --error=pfllib-c10.err
+#SBATCH --gres=gpu:1
+
+export TMPDIR=/data/$USER/
+export HOME=/data/$USER/
+export http_proxy=http://squid.auckland.ac.nz:3128
+export https_proxy=http://squid.auckland.ac.nz:3128
+export PYTHONUNBUFFERED=1
+
+source /data/$USER/miniconda3/etc/profile.d/conda.sh
+conda activate pfllib
+
+cd /data/$USER/disaster-pfl-pfllib-eval/system
+
+python -u main.py \
+  -data Cifar10 \
+  -m CNN \
+  -algo FedAvg \
+  -gr 20 \
+  -ls 1 \
+  -nc 20 \
+  -jr 1.0 \
+  -t 1 \
+  -did 0 \
+  -dev cuda
